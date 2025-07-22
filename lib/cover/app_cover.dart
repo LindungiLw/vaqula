eb5e3bafd673581e249246aac60f571afe277cce
RahmaLindungiLaowo <rahma23@jiu.ac>
2025-07-22 11:27:30 +0700

    project exam

---
+++ lib/cover/app_cover.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:voqula/untils/size_config.dart';
+import 'package:voqula/cover/welcoming_app.dart';
+
+class AppCover extends StatefulWidget {
+  @override
+  _AppCoverState createState() => _AppCoverState();
+}
+
+class _AppCoverState extends State<AppCover> {
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => WelcomingApp()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext buildContext) {
+    SizeConfig().init(context);
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            'assets/cover/cover_app.jpg',
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+            alignment: Alignment.center,
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: SizeConfig.blockHeight * 20),
+                  Text(
+                    'VOQULA',
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+
+                      shadows: [
+                        Shadow(
+                          color: Colors.black,
+                          offset: Offset(3.0, 3.0),
+                          blurRadius: 3.0,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
