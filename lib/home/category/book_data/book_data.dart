b8af0ebae8128765f0997d469c9a7ecc9fbdfc6e
RahmaLindungiLaowo <rahma23@jiu.ac>
2025-08-04 20:47:42 +0700

    project exam

--- lib/home/category/book_data/book_data.dart
+++ lib/home/category/book_data/book_data.dart
@@ -1,5 +1,3 @@
-// lib/data/categories/horror_books.dart
-
 final List<Map<String, String>> horrorBooksData = [
   {
     'imageUrl': 'https://cdn.pixabay.com/photo/2016/11/19/00/30/book-1837012_1280.jpg',
@@ -7,5 +5,4 @@ final List<Map<String, String>> horrorBooksData = [
     'author': 'Author Name',
     'category': 'Horror',
   },
-  // Tambahkan lebih banyak buku 'Horror' di sini
 ];
