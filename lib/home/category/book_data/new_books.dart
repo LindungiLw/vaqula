e738a400bdeb04f4fed105b62ce43d70513242a7
RahmaLindungiLaowo <rahma23@jiu.ac>
2025-08-19 10:00:57 +0700

    Merge remote-tracking branch 'origin/main'

--- lib/home/category/book_data/new_books.dart
+++ lib/home/category/book_data/new_books.dart
@@ -3,7 +3,6 @@ final List<Map<String, String>> newBooksData = [
     'imageUrl': 'assets/books/popular_books/a_new_beginning.jpg',
     'title': 'New Beginnings',
     'author': 'Brenda Kennedy',
-    'author': 'Brenda Kennedy',
     'category': 'New',
   },
   {
@@ -15,8 +14,6 @@ final List<Map<String, String>> newBooksData = [
   {
     'imageUrl': 'assets/books/popular_books/fresh_start.jpg',
     'title': 'Fresh Start',
-    'author': 'Joel Os teen',
-
     'author': 'Joel Os teen',
     'category': 'New',
   },
